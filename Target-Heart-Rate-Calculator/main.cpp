11efc75a5bf70fe0bae19c3b7b0d6f7e6a90ffec
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-10-22 13:28:04 -0700

    Initial Commit

---
+++ Target-Heart-Rate-Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Target-Heart-Rate-Calculator
+//
+//  Created by Amanpreeet Sandhu on 2024-10-22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
